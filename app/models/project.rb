3d4c45c577ac45804b28a7e95f9f30e267ba6380
Jonathan Ballard <jonathan.ballard@gmail.com>
2013-12-04 20:01:06 -0500

    Locate projects by user.

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,4 +2,9 @@ class Project < ActiveRecord::Base
   belongs_to :client
   has_many   :invoices
   attr_accessible :client_id, :project_name, :project_type, :due_date
+
+  scope :by_user, lambda { |user|
+    joins("LEFT JOIN clients ON projects.client_id = clients.id").
+    where("clients.user_id = ?", user)
+  }
 end
