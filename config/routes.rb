5f6bb1a546d0d0e89545ec7b878ddaeff1117232
Jonathan Ballard <jonathan.ballard@gmail.com>
2013-12-04 20:02:41 -0500

    Update routes.

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Devops::Application.routes.draw do
   match '/s/:invoice_key',      :to => "invoice#send_invoice",  :as => "send_invoice"
   match '/invoice/:invoice_key',:to => "invoice#edit",  :as => "invoice_edit"
   match '/p/:project_id',       :to => "project#edit",  :as => "project_edit"
+  match '/about',               :to => "about#index",   :as => "about"
   match '/dashboard',           :to => "index#index",   :as => "dashboard" 
   root :to => "index#index", :as => "dashboard"
 end
