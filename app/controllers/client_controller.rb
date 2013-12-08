3f3913d2aac08cd4f7a040768d3c534c82a7a367
Jonathan Ballard <jonathan.ballard@gmail.com>
2013-12-07 19:21:37 -0500

    Pass user_id in when creating a new contact.

--- app/controllers/client_controller.rb
+++ app/controllers/client_controller.rb
@@ -2,7 +2,8 @@ class ClientController < ApplicationController
   before_filter :authenticate_user!
 
   def new
-    @client = Client.new
+    @client       = Client.new
+    @current_user = current_user
   end
 
   def create
