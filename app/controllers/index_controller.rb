1f53a431857f3bfbe84de0f13743e22034b7262e
Jonathan Ballard <jonathan.ballard@gmail.com>
2013-12-07 19:23:00 -0500

    Get all clients from Stripe and parse through them by user.

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -2,7 +2,8 @@ class IndexController < ApplicationController
   before_filter :authenticate_user!
 
   def index
-    @projects = Project.by_user(current_user.id)
+    clients  = Stripe::Customer.all.data
+    @clients = Client.with_contact_by_customer_keys(clients.collect{|i| i['id']})
   end
   
 end
