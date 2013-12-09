c62ff04a3b492cc3d64412bf318a92e9b6e0dd0d
Jonathan Ballard <jonathan.ballard@gmail.com>
2013-12-08 20:35:04 -0500

    Add contact.phone to scope.

--- app/models/client.rb
+++ app/models/client.rb
@@ -8,14 +8,14 @@ class Client < ActiveRecord::Base
   validates_uniqueness_of :client_name
 
   scope :with_contact_by_user_and_customer_keys, lambda {|user,keys|
-    select('clients.id, clients.customer_key, clients.client_name, contacts.first_name, contacts.last_name, contacts.email').
+    select('clients.id, clients.customer_key, clients.client_name, contacts.first_name, contacts.last_name, contacts.email, contacts.phone').
     joins("LEFT JOIN contacts ON clients.id = contacts.client_id").
     where("clients.user_id = ?",user).
     where("clients.customer_key IN (?)", keys)
   }
 
   scope :with_contact_info_by_user_and_client_id, lambda {|user,client_id|
-    select('clients.id, clients.customer_key, clients.client_name, contacts.first_name, contacts.last_name, contacts.email').
+    select('clients.id, clients.customer_key, clients.client_name, contacts.first_name, contacts.last_name, contacts.email, contacts.phone').
     joins("LEFT JOIN contacts ON clients.id = contacts.client_id").
     where("clients.user_id = ?", user).
     where("clients.id = ?", client_id)
