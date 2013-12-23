12f0fd739802ad7a3066a8ffb1f1430ae0db7c00
Jonathan Ballard <jonathan.ballard@gmail.com>
2013-12-23 17:28:53 -0500

    Update invoices ( w/stripe )

--- app/controllers/invoice_controller.rb
+++ app/controllers/invoice_controller.rb
@@ -36,6 +36,27 @@ class InvoiceController < ApplicationController
   end
 
   def update
+    params[:invoice][:items] = params[:invoice].delete(:invoice_items_attributes)
+    invoice = Invoice.find(params[:id])
+    begin 
+      params[:invoice][:items].each do |k,v|
+        if v.has_key?(:id)
+          item   = InvoiceItems.find(v[:id])
+          x_item = Stripe::InvoiceItem.retrieve(item.item_key)
+          x_item.amount = (v[:amount].to_f*100).to_i
+          x_item.description = v[:description]
+          x_item.save
+          item.update_attributes(v.except(:_destroy, :id))
+        else
+          item = Stripe::InvoiceItem.create(:customer  => invoice.client.customer_key, :amount    => (v[:amount].to_f*100).to_i, 
+            :currency  => 'usd', :description=>v[:description])
+          InvoiceItems.create(:invoice_id => params[:id], :amount => v[:amount],:title => v[:title], :description => v[:description], :item_key => item.id)      
+        end
+      end
+    rescue
+      flash[:error] = 'There was an error updating your request.'
+    end
+    redirect_to :dashboard
   end
 
   def destroy 
