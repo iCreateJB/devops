aecf232d636bd85528a31ccaf83f37b8b2f5c662
Jonathan Ballard <jonathan.ballard@gmail.com>
2013-12-22 20:00:08 -0500

    Clean up .

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,6 @@ Devops::Application.routes.draw do
   match '/c/:client_id',        :to => "invoice#list",  :as => "list_invoice"
   match '/i/:invoice_key',      :to => "invoice#show",  :as => "invoice"
   match '/s/:invoice_key',      :to => "invoice#send_invoice",  :as => "send_invoice"
-  # match '/invoice/:id',:to => "invoice#edit",  :as => "invoice_edit"
   match '/p/:project_id',       :to => "project#edit",  :as => "project_edit"
   match '/about',               :to => "about#index",   :as => "about"
   match '/dashboard',           :to => "index#index",   :as => "dashboard" 
